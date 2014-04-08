98f32c05d0ab1e26e4eb0548f5d8055b97822ec9
profmaad <prof.maad@lambda-bb.de>
2014-04-08 15:56:53 +0800

    Implement correlated stdnormal float prng

--- kernels/random.h
+++ kernels/random.h
@@ -1,5 +1,8 @@
 /* (c) 2014 Maximilian Gerhard Wolter (2009956434) */
 
+# ifndef OPENCL_RANDOM_H
+# define OPENCL_RANDOM_H
+
 # include <mwc64x/mwc64x/mwc64x_rng.cl>
 
 # define M_PI_F       3.14159274101257f
@@ -15,6 +18,13 @@ typedef struct stdnormal_float_prng_state
 	bool has_unused_value;
 	float unused_value;
 } stdnormal_float_prng_state;
+
+typedef struct correlated_stdnormal_float_prng_state
+{
+	stdnormal_float_prng_state *base_state;
+	unsigned int size;
+	float *correlation_matrix;
+} correlated_stdnormal_float_prng_state;
 
 void initialize_uniform_int_prng(uniform_int_prng_state *state, uint2 seed)
 {
@@ -59,3 +69,31 @@ float stdnormal_float_random(stdnormal_float_prng_state *state)
 		return r * cosine;
 	}
 }
+
+void initialize_correlated_stdnormal_float_prng(correlated_stdnormal_float_prng_state *state, stdnormal_float_prng_state *base_state, unsigned int size, float *correlations)
+{
+	state->base_state = base_state;
+	state->size = size;
+	state->correlation_matrix = correlations;
+}
+void correlated_stdnormal_float_random(correlated_stdnormal_float_prng_state *state, float *random)
+{
+	// corr_rand = rand * correlations (vector = vector * matrix)
+
+	float uncorrelated_random[state->size];
+	for(int i = 0; i < state->size; i++)
+	{
+		uncorrelated_random[i] = stdnormal_float_random(state->base_state);
+	}
+
+	for(int i = 0; i < state->size; i++)
+	{
+		random[i] = 0.0f;
+		for(int j = 0; j < state->size; j++)
+		{
+			random[i] += uncorrelated_random[j] * state->correlation_matrix[j*state->size + i];
+		}
+	}
+}
+
+# endif /*OPENCL_RANDOM_H*/
